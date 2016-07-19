8e73dfcca790e98783473b2f14bd00cc75b42ae2
Michael Amos <mikeamos@gmail.com>
2016-07-19 15:18:22 -0700

    client /server pattern

--- app.js
+++ app.js
@@ -2,30 +2,47 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+// var people = require('./people.js');
+
+var people = {people:[]};
+
 server.listen(3000);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var connections = [];
+app.get('/client', function (req, res) {
+  res.sendFile(__dirname + '/client.html');
+});
 
+
+var allClients = [];
 io.on('connection', (socket) => {
-  setInterval(() => {socket.emit('news')}, 300);
-  socket.on('my other event', function (data) {
-    
+  console.log('connecting', socket.id);
+  socket.emit('CUSTOMER_LIST', people);
+  socket.on('NEW_CUSTOMER', handleNewCustomer.bind(null, socket));
+
+  socket.on('disconnect', () => {
+    var toRemove = [];
+    people.people.forEach((person, i) => {
+      if (person.socketId === socket.id) {
+        toRemove.push(i);
+      }
+    });
+    // this is kinda hacky
+    toRemove.forEach(index => {
+      people.people.splice(index, 1);
+    });
+    io.emit('CUSTOMER_LIST_CHANGED', people);
   });
 });
 
-io.on('disconnect', (socket) => {
+function handleNewCustomer(socket, data) {
+  data.socketId = socket.id;
+  people.people.push(data);
+  io.emit('CUSTOMER_LIST_CHANGED', people);
+}
 
-});
-
-
-
-// io.sockets.map((e) => {
-//   console.log(e);
-// })
-// setInterval(() => {console.log('hi')}, 300);
 
 
---
+++ people.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "people": [
+      {
+        "id": 1,
+        "name": "Michael Amos", 
+        "dob": "1/1/1989",
+        "avatar": "someUrl" 
+      },
+      {
+        "id": 2,
+        "name": "Big Bob", 
+        "dob": "1/1/1969",
+        "avatar": "someUrl" 
+      },
+      {
+        "id": 3,
+        "name": "Lucille", 
+        "dob": "1/1/1942",
+        "avatar": "someUrl" 
+      }
+    ]
+  }
